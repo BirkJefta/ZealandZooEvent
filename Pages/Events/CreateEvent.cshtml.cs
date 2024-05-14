448c72e81fa96cb466260e0dfb03e1a0c0023e55
Kwabsnti <dan001@edu.zealand.dk>
2024-05-14 12:52:14 +0200

    EVENT

--- Pages/Events/CreateEvent.cshtml.cs
+++ Pages/Events/CreateEvent.cshtml.cs
@@ -1,11 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandZooEvent.Models;
 
 namespace ZealandZooEvent.Pages.Events;
 
 public class CreateEvent : PageModel
 {
-    public void OnGet()
+    private FakeEventRepository repo;
+
+    [BindProperty] public Event Event { get; set }
+
+    public CreateEvent()
     {
-        
+        repo = new FakeEventRepository();
     }
+
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+
+    public IActionResult OnPost()
+}
+
 }
