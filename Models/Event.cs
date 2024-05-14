0095b97955f1df191900aebd9afe7eee6abac76a
Casper Hollensberg Kiens <44415938+CHKiens@users.noreply.github.com>
2024-05-14 11:33:02 +0200

    Added Event razorpage

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,6 +5,7 @@ namespace ZealandZooEvent.Models
     public class Event
     {
         private string Location { get; set; }
+        private int Id { get; set; }
         private double Price { get; set; }
         private string Name { get; set; }
         private string Description { get; set; }
---
+++ Pages/Events/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace ZealandZooEvent.Pages.Events
+{
+    public class IndexModel : PageModel
+    {
+        private FakeEventRepository repo;
+
+        public List<Event> Events { get; private set; }
+
+        public IndexModel()
+        {
+            repo = new FakeEventRepository;
+        }
+        public void OnGet()
+        {
+            Events = repo.GetAllEvents();
+        }
+    }
+}
