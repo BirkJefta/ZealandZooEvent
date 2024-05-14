133a74956a728f7df078f7f24f58012c1c74979d
Casper Hollensberg Kiens <44415938+CHKiens@users.noreply.github.com>
2024-05-14 10:57:27 +0200

    Added Student class

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ZealandZooEvent.Models
+{
+    public class Student
+    {
+        private string Name { get; set; }
+
+        private string Email { get; set; }
+
+        private string Telephone { get; set;}
+    }
+}
