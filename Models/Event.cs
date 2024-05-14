4de3e75c007585527c7722630e12b7f6bb2f8d83
Casper Hollensberg Kiens <casperkiens@gmail.com>
2024-05-14 10:40:53 +0200

    Added Event class

---
+++ Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZealandZooEvent.Models
+{
+    public class Event
+    {
+        private string Location { get; set; }
+        private double Price { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private DateTime Time { get; set; }
+
+
+    }
+}
