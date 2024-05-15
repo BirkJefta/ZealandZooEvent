f6d47636c9299bc4c18a8eec464044081e7226d3
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-15 17:29:42 +0200

    Tilføjet getevent, updateevents og getallevents

--- Services/JsonEventRepository.cs
+++ Services/JsonEventRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ZealandZooEvent.Helpers;
 using ZealandZooEvent.Interfaces;
 using ZealandZooEvent.Models;
@@ -10,6 +11,37 @@ namespace ZealandZooEvent.Services {
         public List<Event> GetAllEvents()
         {
             return JsonFileReader.ReadToJson(JsonFileName);
+        }
+        public Event GetEvent(int id)
+        {
+            foreach (var v in GetAllEvents())
+            {
+                if (v.Id == id)
+                {
+                    return v;
+                }
+            }
+            return new Event();
+        }
+        public void UpdateEvent(Event @evt)
+        {
+            List<Event> @events = GetAllEvents().ToList();
+            if (@evt != null)
+            {
+                foreach (var e in GetAllEvents())
+                {
+                    if (e.Id == @evt.Id)
+                    {
+                        e.Id = evt.Id;
+                        e.Name = evt.Name;
+                        e.Price = evt.Price;
+                        e.Description = evt.Description;
+                        e.Time = evt.Time;
+                        e.Location = evt.Location;
+                    }
+                }
+            }
+            JsonFileWriter.WriteToJson(@events, JsonFileName);
         }
     }
 }
