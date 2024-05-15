cac1021dc77297ae0b24dcda441c5668503c0132
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-15 17:10:39 +0200

    Helpers Folder er tilføjet

---
+++ Services/JsonEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ZealandZooEvent.Interfaces;
+using ZealandZooEvent.Models;
+
+namespace ZealandZooEvent.Services {
+    public class JsonEventRepository:IRepository
+    {
+        string JsonFileName = @"C:\Users\birko\Desktop\Datamatiker\Obligatoriske opgaver\15-5-2024\Data\JsonEvents.json";
+        public static List<Event> GetAllEvents()
+        {
+
+        }
+    }
+}
