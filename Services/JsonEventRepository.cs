2ee381fec958cd3deb0b2f29e55a58e74b3e759c
Casper Hollensberg Kiens <44415938+CHKiens@users.noreply.github.com>
2024-05-16 09:59:51 +0200

    Fixed Json path

--- Services/JsonEventRepository.cs
+++ Services/JsonEventRepository.cs
@@ -7,7 +7,7 @@ using ZealandZooEvent.Models;
 namespace ZealandZooEvent.Services {
     public class JsonEventRepository:IRepository
     {
-        string JsonFileName = @"C:\Users\birko\Desktop\Datamatiker\Obligatoriske opgaver\15-5-2024\Data\JsonEvents.json";
+        string JsonFileName = @"Data\JsonEvents.json";
         public List<Event> GetAllEvents()
         {
             return JsonFileReader.ReadToJson(JsonFileName);
