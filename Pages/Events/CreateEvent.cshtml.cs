34e430665014e333e3e74ebbc416ab16e44c831f
Finsby1 <bej002@edu.zealand.dk>
2024-05-16 10:29:17 +0200

    asdasd

--- Pages/Events/CreateEvent.cshtml.cs
+++ Pages/Events/CreateEvent.cshtml.cs
@@ -1,11 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandZooEvent.Models;
+using ZealandZooEvent.Services;
 
 namespace ZealandZooEvent.Pages.Events;
 
 public class CreateEvent : PageModel
 {
-    public void OnGet()
+    private FakeEventRepository repo;
+
+    [BindProperty] public Event Event { get; set; }
+
+    public CreateEvent()
     {
-        
+        repo = new FakeEventRepository();
     }
+
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        repo.AddEvent(Event);
+        return RedirectToPage("Index");
+    } 
 }
