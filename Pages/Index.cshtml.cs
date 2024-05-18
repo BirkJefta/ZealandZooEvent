c332e94c50feb321edfc59edf050926f548d7a92
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-18 11:48:09 +0200

    Tilføjet en karrousel med billeder fra event vil tilføje pile, overskrift og pile

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,19 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZealandZooEvent.Interfaces;
+using ZealandZooEvent.Models;
 
 namespace ZealandZooEvent.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IRepository _repository;
+        
+        [BindProperty]
+        public List<Event> Events { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IRepository repository)
         {
+            _repository = repository;
             _logger = logger;
+            
         }
 
         public void OnGet()
         {
-
+            Events = _repository.GetAllEvents();
         }
     }
 }
