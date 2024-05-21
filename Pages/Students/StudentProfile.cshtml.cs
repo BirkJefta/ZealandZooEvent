300020508e4690ab4050c1a2679284e16fe781de
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-21 20:25:21 +0200

    Starten på en lang rejse

--- Pages/Students/StudentProfile.cshtml.cs
+++ Pages/Students/StudentProfile.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandZooEvent.Interfaces;
+using ZealandZooEvent.Models;
 
 namespace ZealandZooEvent.Pages.Students
 {
     public class StudentProfileModel : PageModel
     {
+        IStudentRepository _studentRepository;
+        [BindProperty]
+        public Student student {get; set; }
+        public StudentProfileModel(IStudentRepository studentRepository) {
+            _studentRepository = studentRepository;
+            student = studentRepository.LoggedInStudent();
+        }
         public void OnGet()
         {
+
         }
     }
 }
