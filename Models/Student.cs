fbf82017cc84560866b9e0bd3af6dd774bd6f50a
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-21 18:59:07 +0200

    Lavede nogle krav til oprettellse af bruger

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,17 @@
-﻿namespace ZealandZooEvent.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZealandZooEvent.Models
 {
     public class Student
     {
         public int Id { get; set; }
         public string Name { get; set; }
- 
+        [Required(ErrorMessage ="Must enter password")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Must enter Email")]
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Phone]
         public string Telephone { get; set; }
 
         public bool isAdmin { get; set; }
