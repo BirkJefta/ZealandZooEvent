23618f332e5e9e92e1cca0a3f3a8666fe8759060
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-21 13:42:48 +0200

    Her laves den version en admin ser

---
+++ Pages/Events/AdminPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZooEvent.Pages.Events
+{
+    public class AdminPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
