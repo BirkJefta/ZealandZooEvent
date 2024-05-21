1e71ca4ef448ca245e8c73262ddcf0534e3c8b4e
Finsby1 <bej002@edu.zealand.dk>
2024-05-21 09:46:08 +0200

    xdd

--- Pages/Events/OpeningHours.cshtml.cs
+++ Pages/Events/OpeningHours.cshtml.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ZealandZooEvent.Pages {
+namespace ZealandZooEvent.Pages.Events {
     public class PrivacyModel : PageModel {
         private readonly ILogger<PrivacyModel> _logger;
 
