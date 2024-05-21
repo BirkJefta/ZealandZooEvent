69d34a5d1ca0f1544704b198ddf8caf5a5e9a34d
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-21 13:46:50 +0200

    tilføjet html

--- Pages/Events/AdminPage.cshtml.cs
+++ Pages/Events/AdminPage.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using ZealandZooEvent.Interfaces;
+using ZealandZooEvent.Models;
 
 namespace ZealandZooEvent.Pages.Events
 {
     public class AdminPageModel : PageModel
     {
+        IRepository repo;
+        public List<Event> Events { get; private set; }
+        [BindProperty] public string FilterCriteria { get; set; }
+        public AdminPageModel(IRepository repository) 
+        {
+            repo= repository;
+        }
         public void OnGet()
         {
+            Events = repo.GetAllEvents();
+        }
+        public void OnPost()
+        {
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Events = repo.FilterEvents(FilterCriteria);
+            }
+            else
+            {
+                Events = repo.GetAllEvents();
+            }
         }
     }
 }
