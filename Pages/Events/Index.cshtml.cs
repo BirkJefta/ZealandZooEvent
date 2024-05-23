57441547fccd5e3215b7f23ad1785f14aa1dda05
Sebastian Nybo Hansen <seh001@edu.zealand.dk>
2024-05-23 18:19:23 +0200

    Filter knap med pris og datosøgning

--- Pages/Events/Index.cshtml.cs
+++ Pages/Events/Index.cshtml.cs
@@ -14,26 +14,54 @@ namespace ZealandZooEvent.Pages.Events
         IRepository repo;
 
         public List<Event> Events { get; private set; }
-        [BindProperty]public string FilterCriteria { get; set; }
+        [BindProperty] public string FilterCriteria { get; set; }
+        [BindProperty] public DateTime? StartDate { get; set; }
+        [BindProperty] public DateTime? EndDate { get; set; }
+        [BindProperty] public string SortCriteria { get; set; }
 
         public IndexModel(IRepository repository)
         {
             repo = repository;
         }
+
         public void OnGet()
         {
             Events = repo.GetAllEvents();
         }
+
         public void OnPost()
         {
-            if(!string.IsNullOrEmpty(FilterCriteria))
+            Events = repo.GetAllEvents();
+
+            if (!string.IsNullOrEmpty(FilterCriteria))
             {
                 Events = repo.FilterEvents(FilterCriteria);
             }
-            else
+
+            if (StartDate.HasValue && EndDate.HasValue)
             {
-                Events = repo.GetAllEvents();
+                Events = Events.Where(e => e.Time.Date >= StartDate.Value.Date && e.Time.Date <= EndDate.Value.Date).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(SortCriteria))
+            {
+                switch (SortCriteria)
+                {
+                    case "PriceDesc":
+                        Events = Events.OrderByDescending(e => e.Price).ToList();
+                        break;
+                    case "PriceAsc":
+                        Events = Events.OrderBy(e => e.Price).ToList();
+                        break;
+                    case "DateAsc":
+                        Events = Events.OrderBy(e => e.Time).ToList();
+                        break;
+                    case "DateDesc":
+                        Events = Events.OrderByDescending(e => e.Time).ToList();
+                        break;
+                }
             }
         }
     }
+
 }
