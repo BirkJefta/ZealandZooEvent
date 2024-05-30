138039eb704172c050bae894860a4fb0cb01f104
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-05-30 09:51:32 +0200

    Rettet lille fejl

--- Pages/Events/ViewEvent.cshtml.cs
+++ Pages/Events/ViewEvent.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
 using ZealandZooEvent.Interfaces;
 using ZealandZooEvent.Models;
 
